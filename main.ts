8f7b57732b3eb59796c034c82802195fb764aab7
Jesse Vendt <jvendt@ebsco.com>
2025-06-12 18:54:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-radio.onReceivedString(function () {
-    maqueen.motorStop(maqueen.Motors.M1)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-})
-radio.onReceivedString(function () {
-    maqueen.motorStop(maqueen.Motors.M2)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-})
-radio.onReceivedString(function () {
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "right") {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        if (receivedString == "left") {
+            maqueen.motorStop(maqueen.Motors.M2)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+            if (receivedString == "horn") {
+                music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
+            }
+        }
+    }
 })
 radio.setGroup(1)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
