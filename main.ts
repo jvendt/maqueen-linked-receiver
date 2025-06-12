dc39b46c5b31c9e471128db2c8803e44752b2db5
Jesse Vendt <jvendt@ebsco.com>
2025-06-12 19:21:08 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,17 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "right") {
         maqueen.motorStop(maqueen.Motors.M1)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-    } else {
+        basic.pause(100)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else {
         if (receivedString == "left") {
             maqueen.motorStop(maqueen.Motors.M2)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        } else {
+            basic.pause(100)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        } else {
             if (receivedString == "horn") {
-                music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
+                music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
             }
         }
     }
